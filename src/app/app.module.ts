736a3a1a89f5aac7284c5e4f996f89904cc72e26
Renan <renan.cav.ti@gmail.com>
2020-02-25 00:18:30 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FeedPageModule } from '../pages/feed/feed.module';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
@@ -21,7 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    FeedPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
